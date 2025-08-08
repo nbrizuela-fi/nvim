94a4b5b56843fab59813822745bf66b3c9662acd
Natanael <nbrizuela@fi.uba.ar>
2025-08-08 01:51:14 -0300

    keymaps-options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+--para copiar y pegar fuera de nvim
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
