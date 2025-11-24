4faa2efe072bd370fec26a25e1593a7ae1d91fcb
Natanael <nbrizuela@fi.uba.ar>
2025-11-24 14:39:56 -0300

    se agrego visualizador de pdf

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,26 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 --para copiar y pegar fuera de nvim
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+vim.keymap.set(
+  "n",
+  "<leader>jj",
+  "<cmd>lua require('pdfview.renderer').next_page()<CR>",
+  { desc = "PDFview: Next Page" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>kk",
+  "<cmd>lua require('pdfview.renderer').previous_page()<CR>",
+  { desc = "PDFview: Previous page" }
+)
+
+vim.keymap.set("n", "<leader>pp", function()
+  local pdf_path = vim.api.nvim_buf_get_name(0)
+  if pdf_path == "" then
+    print("No hay PDF abierto")
+    return
+  end
+  require("pdfview").open(pdf_path)
+end, { desc = "Open current PDF via pdfview" })
